525c51835cec14868ab1012f8f712fab5c5df16c
KIM MINJAE <minjae2717@gmail.com>
2023-08-07 09:13:16 +0900

    [update] ACE1010 기능추가
    DataGrid까지 구현

--- front/src/components/douzonecontainer/DouzoneContainer.js
+++ front/src/components/douzonecontainer/DouzoneContainer.js
@@ -9,6 +9,72 @@ import {
   Slide,
 } from '@mui/material';
 import "./douzonecontainer.css"
+import { createTheme } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+
+const douzonecontainertheme = createTheme({
+  components: {
+    MuiListItemText: {
+      styleOverrides: {
+        primary: {
+          fontSize: '15px',
+          fontWeight: 'bold',
+          height: '15px',
+          lineHeight: '15px'
+        },
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          height: "30px",
+          backgroundColor: "#FBFBFB",
+          color: "black",
+        }
+      },
+      defaultProps: {
+        variant: "contained",
+        color: "primary",
+        fullWidth: true,
+      }
+    },
+    MuiGrid: {
+      styleOverrides: {
+        root: {  // 모든 Grid 태그에 적용하려면 root를 사용하세요.
+          // borderBottom: '1px solid black',
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: {
+          '&:hover $notchedOutline': {
+            borderColor: 'rgba(0, 0, 0, 0.23)', // 기본 테두리 색상으로 유지
+          },
+          '&.Mui-focused $notchedOutline': {
+            borderColor: 'rgba(0, 0, 0, 0.23)', // 기본 테두리 색상으로 유지
+          },
+          borderRadius: 0,
+        },
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          '&:hover ': {
+            cursor: 'pointer',
+            border: '1px solid #0f8bff',
+          }, '&.noHoverEffect:hover': {
+            cursor: 'auto',
+            border: 'none',
+          },
+        },
+      },
+    }
+
+
+  },
+});
 class DouzoneContainer extends Component {
   constructor(props) {
     super(props);
@@ -59,87 +125,89 @@ class DouzoneContainer extends Component {
     }
     //const showDelete = this.props.onDelete
     return (
-      <div className="douzone-container">
-        <div className="container-header">
-          <div className="container-header-left" >
-            {this.props.title}
-          </div>
-          <div className="container-header-right">
-            <div className="functionButton">
-              <Button
-                id="basic-button"
-                // aria-controls={open ? 'basic-menu' : undefined}
-                // aria-haspopup="true"
-                // aria-expanded={open ? 'true' : undefined}
-                onClick={this.handleClick}
-              >
-                기능모음
-              </Button>
-              <Menu
-                id="basic-menu"
-                anchorEl={this.state.funcVowel}
-                open={Boolean(this.state.funcVowel)}
-                onClose={this.handleClose}
-                MenuListProps={{
-                  'aria-labelledby': 'basic-button',
-                }}
-              >
-                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                <MenuItem onClick={this.handleClose}>My account</MenuItem>
-                <MenuItem onClick={this.handleClose}>Logout</MenuItem>
-              </Menu>
+      <ThemeProvider theme={douzonecontainertheme}>
+        <div className="douzone-container">
+          <div className="container-header">
+            <div className="container-header-left" >
+              {this.props.title}
             </div>
-            <span style={{ color: 'lightgrey' }}>|</span>
-            <DeleteIcon style={{ display: showDelete }} className="deleteIcon" onClick={this.props.delete} sx={{ fontSize: 30 }}></DeleteIcon>
+            <div className="container-header-right">
+              <div className="functionButton">
+                <Button
+                  id="basic-button"
+                  // aria-controls={open ? 'basic-menu' : undefined}
+                  // aria-haspopup="true"
+                  // aria-expanded={open ? 'true' : undefined}
+                  onClick={this.handleClick}
+                >
+                  기능모음
+                </Button>
+                <Menu
+                  id="basic-menu"
+                  anchorEl={this.state.funcVowel}
+                  open={Boolean(this.state.funcVowel)}
+                  onClose={this.handleClose}
+                  MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                  }}
+                >
+                  <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                  <MenuItem onClick={this.handleClose}>Logout</MenuItem>
+                </Menu>
+              </div>
+              <span style={{ color: 'lightgrey' }}>|</span>
+              <DeleteIcon style={{ display: showDelete }} className="deleteIcon" onClick={this.props.delete} sx={{ fontSize: 30 }}></DeleteIcon>
 
 
+            </div>
           </div>
-        </div>
 
-        <div className="children" style={{ marginTop: '10px' }}>
-          {this.props.children}
-        </div>
+          <div className="children" style={{ marginTop: '10px' }}>
+            {this.props.children}
+          </div>
 
-        <Dialog
-          open={openDeleteModal}
-          onClose={handleClose}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              {message}
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>아니오</Button>
-            <Button onClick={handleConfirm} autoFocus>
-              네
-            </Button>
-          </DialogActions>
-        </Dialog>
+          <Dialog
+            open={openDeleteModal}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+          >
+            <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                {message}
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>아니오</Button>
+              <Button onClick={handleConfirm} autoFocus>
+                네
+              </Button>
+            </DialogActions>
+          </Dialog>
 
-        <Snackbar
-          open={this.state.openSnackBar}
-          autoHideDuration={2000}
-          onClose={this.handleSnackbarClose}
-          TransitionComponent={Slide}
-          anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-        >
-          <Alert onClose={this.handleSnackbarClose} severity={this.state.severity} sx={{
-            width: '100%',
-            bgcolor: backgroundColor,
-            '.MuiAlert-icon': {
-              color: iconColor
-            },
-            color: 'white',
-            fontWeight: 'bold',
-          }}>
-            {this.state.snackBarMessage}
-          </Alert>
-        </Snackbar>
-      </div >
+          <Snackbar
+            open={this.state.openSnackBar}
+            autoHideDuration={2000}
+            onClose={this.handleSnackbarClose}
+            TransitionComponent={Slide}
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
+          >
+            <Alert onClose={this.handleSnackbarClose} severity={this.state.severity} sx={{
+              width: '100%',
+              bgcolor: backgroundColor,
+              '.MuiAlert-icon': {
+                color: iconColor
+              },
+              color: 'white',
+              fontWeight: 'bold',
+            }}>
+              {this.state.snackBarMessage}
+            </Alert>
+          </Snackbar>
+        </div >
+      </ThemeProvider>
     )
   }
 }
--- front/src/pages/ace1011/Ace1011.js
+++ front/src/pages/ace1011/Ace1011.js
@@ -2,18 +2,98 @@ import { Component } from "react";
 
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import { DataGrid } from "@mui/x-data-grid";
 
 
 class Ace1011 extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hours: '18',
+      minutes: '00',
+      isOpen: false
+    };
+
+
+    this.handleMinutesChange = this.handleMinutesChange.bind(this);
+    this.toggleOpen = this.toggleOpen.bind(this);
+    this.setWrapperRef = this.setWrapperRef.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('mousedown', this.handleClickOutside);
+  }
+
+  setWrapperRef(node) {
+    this.wrapperRef = node;
+  }
+
+
+  handleClickOutside(event) {
+    if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
+      this.setState({ isOpen: false });
+    }
+  }
+
+  handleHoursChange(e) {
+    this.setState({ hours: e.target.value });
+  }
+
+  handleMinutesChange(e) {
+    this.setState({ minutes: e.target.value });
+  }
+
+  toggleOpen() {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+
   render() {
+    const a = "setWrapperRef"
+    this[a]()
+    const hours = [];
+    for (let i = 0; i <= 23; i++) {
+      hours.push(i < 10 ? '0' + i : '' + i);
+    }
+
+    const minutes = [];
+    for (let i = 0; i <= 59; i++) {
+      minutes.push(i < 10 ? '0' + i : '' + i);
+    }
+
     return (
+      <div ref={this.setWrapperRef}>
+        <div onClick={this.toggleOpen}>
+          현재 시간: {this.state.hours}:{this.state.minutes}
+        </div>
+        {this.state.isOpen && (
+          <div>
+            <label>시간:
+              <select value={this.state.hours} onChange={this.handleHoursChange.bind(this)}>
+                {hours.map((hour, index) =>
+                  <option key={index} value={hour}>{hour}</option>
+                )}
+              </select>
+            </label>
+            <label>분:
+              <select value={this.state.minutes} onChange={this.handleMinutesChange}>
+                {minutes.map((minute, index) =>
+                  <option key={index} value={minute}>{minute}</option>
+                )}
+              </select>
+            </label>
+          </div>
+        )}
 
-      <ListItemButton sx={{ pl: 10 }}>
-        <ListItemText primary="운행기록부(관리용)" primaryTypographyProps={{ fontSize: '15px' }} />
-      </ListItemButton>
 
-    )
+      </div>
+    );
   }
 }
+
 
 export default Ace1011;
